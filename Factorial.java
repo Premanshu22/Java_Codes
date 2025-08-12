06a05c1bec384d2f1e5f6e5740afda2929c0680f
BinaryKnight⚔️ <137209796+Premanshu22@users.noreply.github.com>
2025-08-12 21:06:57 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Factorial {
+    
+    public static void printFactorial(int n)
+    {
+        int fact = 1;
+
+        for(int i=n; i>=1; i--)
+        {
+            fact=fact*i;
+        }
+
+        System.out.println(fact);
+    }
+
+
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        printFactorial(num);
+    }
+}
